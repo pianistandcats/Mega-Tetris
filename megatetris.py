6eeabaac1d8acdea8a95cbdd322a73a603d1d9d4
pianist and cats <catsandpianist@gmail.com>
2024-07-12 11:14:42 -0300

    movimiento basico

--- megatetris.py
+++ megatetris.py
@@ -2,6 +2,7 @@ import pygame
 import numpy as np
 import time
 from piezas import *
+import random
 
 pygame.init()
 
@@ -29,9 +30,53 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                pieza.giro(game_state, nxC)
+                print("Arriba")
+        
+    # Obtener el estado de todas las teclas
+    keys = pygame.key.get_pressed()
+    
+    sleep_time = 0.3
+    if keys[pygame.K_ESCAPE]:
+        running = False
+    if keys[pygame.K_LEFT]:
+        # print("Izquierda")
+        puede = True
+        for esp in pieza.espacios:
+            if game_state[esp[0] + pieza.posicion[0] -1, esp[1] + pieza.posicion[1]] == 1 or esp[0] + pieza.posicion[0] - 1 < 0:
+                puede = False
+                print("toca isquierda")
+                break
+            else:
+                print(str(game_state[esp[0] + pieza.posicion[0] -1,0]), str([esp[0] + pieza.posicion[0] -1,0]))
+            
+        if puede:
+            pieza.posicion[0] -= 1
+    if keys[pygame.K_RIGHT]:
+        # print("Derecha")
+        puede = True
+        for esp in pieza.espacios:
+            if game_state[esp[0] + pieza.posicion[0] +1, esp[1] + pieza.posicion[1]] == 1 or esp[0] + pieza.posicion[0] + 1 >= nxC:
+                puede = False
+                print("toca derecha")
+                break
+            else:
+                print(str(game_state[esp[0] + pieza.posicion[0]]), str([esp[0] + pieza.posicion[0],0]))
+             
+            
+        if puede:
+            pieza.posicion[0] += 1
+        
+    if keys[pygame.K_DOWN]:
+        # print("Abajo")
+        sleep_time = 0
+    
     
     screen.fill(bg)
-    time.sleep(0.1)
+    time.sleep(sleep_time)
     pieza_state =  np.zeros((nxC+7,nyC+7))
     for pos in pieza.espacios:
         pieza_state[pos[0] + pieza.posicion[0],pos[1] + pieza.posicion[1]] = 1 
@@ -47,7 +92,10 @@ while running:
             if game_state[x,y] == 1:
                 pygame.draw.polygon(screen, (255,255,255), poly, 0)
             elif pieza_state[x,y] == 1:
-                pygame.draw.polygon(screen, (0,0,255), poly, 0)
+                if pieza.posicion[0] == x and pieza.posicion[1] == y:
+                    pygame.draw.polygon(screen, (0,255,0), poly, 0)
+                else:
+                    pygame.draw.polygon(screen, (0,0,255), poly, 0)
             else:
                 pygame.draw.polygon(screen, (50,50,50), poly, 1)
     
@@ -55,6 +103,9 @@ while running:
         for pos in pieza.espacios:
             game_state[pos[0] + pieza.posicion[0],pos[1] + pieza.posicion[1]] = 1
         pieza.posicion = np.array([15,0])
+        pieza.tipo = get_all(piezas)[random.randint(0,6)]
+        pieza.espacios = piezas[pieza.tipo]
+        pieza.giros = 2
     
     pygame.display.flip()
 
--- piezas.py
+++ piezas.py
@@ -5,19 +5,57 @@ class Pieza:
         self.posicion = posicion
         self.espacios = piezas[tipo]
         self.tipo = tipo
+        self.giros = 2
+    
+    def giro(self, game_state, nxC):
+        espacios2 = []
+        if self.tipo == "o":
+            return
+        elif self.tipo == "i":
+            for esp in self.espacios:
+                new_espace = np.array([0,0])
+                
+                if self.giros % 2 == 0:
+                    new_espace[1] = esp[0] *-1
+                    new_espace[0] = esp[1] 
+                else:
+                    new_espace[1] = esp[0] 
+                    new_espace[0] = esp[1] *-1
+                    
+                espacios2.append(new_espace)
+                if game_state[esp[0] + self.posicion[0] -1, esp[1] + self.posicion[1]] == 1 or esp[0] + self.posicion[0] - 1 < 0:
+                    return
+                elif game_state[esp[0] + self.posicion[0] +1, esp[1] + self.posicion[1]] == 1 or esp[0] + self.posicion[0] + 1 >= nxC:
+                    return
+            
+        else:
+            for esp in self.espacios:
+                new_espace = np.array([0,0])
+                    
+                new_espace[1] = esp[0]
+                new_espace[0] = esp[1] *-1
+                
+                espacios2.append(new_espace)
+                if game_state[esp[0] + self.posicion[0] -1, esp[1] + self.posicion[1]] == 1 or esp[0] + self.posicion[0] - 1 < 0:
+                    return
+                elif game_state[esp[0] + self.posicion[0] +1, esp[1] + self.posicion[1]] == 1 or esp[0] + self.posicion[0] + 1 >= nxC:
+                    return
+        
+        self.giros += 1
+        self.espacios = espacios2
     
     def mover(self, estado, tx,ty):
         puede = True
         for pos in self.espacios:
-            if estado[pos[0] + self.posicion[0], pos[1] + self.posicion[1] + 1] == 1:
+            if estado[pos[0] + self.posicion[0], pos[1] + self.posicion[1] + 1] == 1 or pos[1] + self.posicion[1] + 1 >= ty:
                 puede = False
                 break
-
+        
         if puede:
-            if self.posicion[1] < ty-1:
+            if pos[1] + self.posicion[1] + 1 < ty:
                 self.posicion[1] += 1
                 return False
-            elif self.posicion[1] == ty-1:
+            elif pos[1] + self.posicion[1] + 1 >= ty:
                 print("piso")
                 return True
         else:
@@ -26,5 +64,16 @@ class Pieza:
             
         
 piezas = {
-    "t": [np.array([0,0]),np.array([1,0]),np.array([-1,0]),np.array([0,-1])]
+    "t": [np.array([0,0]),np.array([1,0]),np.array([-1,0]),np.array([0,-1])],
+    "L": [np.array([0,0]),np.array([1,1]),np.array([0,-1]),np.array([0,1])],
+    "Lin": [np.array([0,0]),np.array([-1,1]),np.array([0,-1]),np.array([0,1])],
+    "i": [np.array([1,0]),np.array([1,1]),np.array([1,-1]),np.array([1,2])],
+    "s": [np.array([0,0]),np.array([0,1]),np.array([1,0]),np.array([-1,1])],
+    "z": [np.array([0,0]),np.array([0,-1]),np.array([1,0]),np.array([1,1])],
+    "o": [np.array([0,0]),np.array([1,0]),np.array([0,1]),np.array([1,1])],
 }
+def get_all(dict):
+    todos = []
+    for key, value in dict.items():
+        todos.append(key)
+    return todos
